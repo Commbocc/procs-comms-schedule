4f07e6e2e255849e5c6cb246224d8f1136a8ff5b
Jordan Pisani <pisanij@hillsboroughcounty.org>
2024-02-12 12:33:35 -0500

    remove comment

--- src/lib/airtable.js
+++ src/lib/airtable.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: 'https://api.airtable.com/v0/appnxWI0G168XdRnz',
-  // commbocc@gmail.com account read only api key
   headers: {
     Authorization: `Bearer patCMqNApPrJE8SXi.143baa8db7f29e133b886a0c24776496d00671e7ac993231593b78a2cfe599a6`,
   },
