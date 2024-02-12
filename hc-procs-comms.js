e46c7927016e1ed4f61e73ee21ac4ead7e2ecbd6
JordanPisani <71664286+JordanPisani@users.noreply.github.com>
2024-02-12 17:33:58 +0000

    Deploying to gh-pages from @ Commbocc/procs-comms-schedule@4f07e6e2e255849e5c6cb246224d8f1136a8ff5b 🚀

--- hc-procs-comms.js
+++ hc-procs-comms.js
@@ -5992,7 +5992,6 @@ te.HttpStatusCode = df;
 te.default = te;
 const Bi = te.create({
   baseURL: "https://api.airtable.com/v0/appnxWI0G168XdRnz",
-  // commbocc@gmail.com account read only api key
   headers: {
     Authorization: "Bearer patCMqNApPrJE8SXi.143baa8db7f29e133b886a0c24776496d00671e7ac993231593b78a2cfe599a6"
   }
