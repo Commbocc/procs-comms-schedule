a717c83091580da6e056eaf4be65eddb15536495
Jordan Pisani <pisanij@hillsboroughcounty.org>
2024-02-12 12:16:56 -0500

    update airtable auth token

--- src/lib/airtable.js
+++ src/lib/airtable.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'https://api.airtable.com/v0/appnxWI0G168XdRnz',
   // commbocc@gmail.com account read only api key
-  headers: { Authorization: `Bearer key1XrFuYhG1GKHvu` },
+  headers: {
+    Authorization: `Bearer patCMqNApPrJE8SXi.143baa8db7f29e133b886a0c24776496d00671e7ac993231593b78a2cfe599a6`,
+  },
 })
